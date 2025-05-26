455d25a28f2854116aef82d5f0ed9c073d0cc464
MatoG50 <matog50@gmail.com>
2025-05-26 19:33:20 +0300

    settings cors update

--- cmvdb/settings.py
+++ cmvdb/settings.py
@@ -133,5 +133,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
      'http://127.0.0.1:8000',
     "http://localhost:5173",
-    'https://cmv-log.vercel.app/',  
+    'https://cmv-log.vercel.app',  
 ]
